703a1d0a8fc2c9bbc88b73f0f8ecfb7468c99c86
Murilo Martins da Silva <murilo.silva@spread.com.br>
2021-05-08 12:52:42 -0300

    Criado teste de dividir e refatoracao

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -1,5 +1,7 @@
 package br.ce.wcaquino.servicos;
 
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
 public class Calculadora {
 
 	public int somar(int a, int b) {
@@ -8,6 +10,13 @@ public class Calculadora {
 
 	public int subtrair(int a, int b) {
 		return a - b;
+	}
+
+	public int dividir(int a, int b) throws NaoPodeDividirPorZeroException {
+		if(b == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return a / b;
 	}
 
 }
--- src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -3,12 +3,22 @@ package br.ce.wcaquino.servicos;
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
 public class CalculadoraTest {
 
+	/**
+	 * Cria-se primeiro o teste
+	 * em seguida as classes e metodos necessarios
+	 * 
+	 */
+	
+	private Calculadora calc;
+	
 	@Test
 	public void somarDoisValores() {
 		int a = 5, b = 3;
-		Calculadora calc = new Calculadora();
+		calc = new Calculadora();
 		
 		int resultado = calc.somar(a, b);
 		
@@ -18,10 +28,29 @@ public class CalculadoraTest {
 	@Test
 	public void subtrairDoisValores() {
 		int a = 8, b = 5;
-		Calculadora calc = new Calculadora();
+		calc = new Calculadora();
 		
 		int resultado = calc.subtrair(a, b);
 		
 		Assert.assertEquals(3, resultado);
+	}
+	
+	@Test
+	public void dividirDoisValores() throws NaoPodeDividirPorZeroException {
+		int a = 6, b = 3;
+		calc = new Calculadora();
+		
+		int resultado = calc.dividir(a, b);
+		
+		Assert.assertEquals(2, resultado);
+	}
+	
+	@Test(expected=NaoPodeDividirPorZeroException.class)
+	public void lancarExceptionAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		int a = 10, b = 0;
+		calc = new Calculadora();
+		
+		calc.dividir(a, b);
+		
 	}
 }
